0322a1e68e0865ac7d7945e4adbf36bbb8ba5291
zzdd1111 <zkaidd@outlook.com>
2025-05-02 14:03:32 +1000

    Delete Exploration_system_test directory

--- Exploration_system_test/Source/Exploration/Exploration.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Exploration.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Exploration, "Exploration" );
-
-DEFINE_LOG_CATEGORY(LogExploration)
- 
